842411da66ed014c86415cf342fa174316f73458
Martin Pliego <martin.pliego@travelperk.com>
2020-06-17 16:10:17 -0300

    Added NavBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Route, Switch, BrowserRouter as Router, Redirect} from 'react-router-dom';
+import {Route, Switch, BrowserRouter as Router, Redirect, Link} from 'react-router-dom';
 import {RecipeDetail} from "./screens/RecipeDetail";
 import {RecipeList} from "./screens/RecipeList";
+import {Nav} from "./components/Nav"
 
 function App() {
     return (
         <div className="App">
             <Router>
+                <Nav.Container key="nav">
+                    <Nav.Link key="recipe-list">
+                        <Link to={'/recipe'}>Recipes</Link>
+                    </Nav.Link>
+                    <Nav.Link key="recipe-new">
+                        <Link to={'/recipe/New'}>New Recipe</Link>
+                    </Nav.Link>
+                </Nav.Container>
                 <Switch>
                     <Route path="/recipe/new" component={RecipeDetail}/>
                     <Route path="/recipe/:id" component={RecipeDetail}/>
